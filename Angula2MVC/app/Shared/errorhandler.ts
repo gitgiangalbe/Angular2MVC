9010cef0901f4f1337dc32368fe15a64a1c029ce
GIANCARLO TOZZI <giancarlo.tozzi@outlook.it>
2018-02-08 14:56:08 +0100

    first

---
+++ Angula2MVC/app/Shared/errorhandler.ts
@@ -0,0 +1,21 @@
+﻿import { ErrorHandler } from '@angular/core';
+
+export default class AppErrorHandler extends ErrorHandler {
+
+    constructor() {
+        // We rethrow exceptions, so operations like 'bootstrap' will result in an error
+        // when an error happens. If we do not rethrow, bootstrap will always succeed.
+        super();
+    }
+
+    handleError(error: any) {
+        debugger;
+        if (error.status == "401") {
+            alert("Non sei loggato, esegui il log-in e torna indietro!");
+        } else {
+            alert(error);
+        }
+        
+        super.handleError(error);
+    }
+}
---
+++ Angula2MVC/app/app.module.ts
@@ -0,0 +1,26 @@
+﻿import 'hammerjs';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { UserComponent } from './components/user.component';
+import { HomeComponent } from './components/home.component';
+import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
+import { UserService } from './Service/user.service';
+import { UserFilterPipe } from './Filter/user.pipe';
+import { SearchComponent } from './Shared/search.component';
+import AppErrorHandler from './Shared/errorhandler';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material';
+
+@NgModule({
+    imports: [BrowserModule, ReactiveFormsModule, HttpModule, routing, Ng2Bs3ModalModule, FormsModule ],
+    declarations: [AppComponent, UserComponent, HomeComponent, UserFilterPipe, SearchComponent],
+    providers: [{provide: ErrorHandler, useClass: AppErrorHandler }, { provide: APP_BASE_HREF, useValue: '/' }, UserService],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
